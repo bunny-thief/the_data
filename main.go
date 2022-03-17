ea9f7f0161d16e17d6be3280e17d3c23c31222c1
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2022-03-17 13:00:26 +0000

    IsPalindrome passes tests

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bunny-thief/the_data/palindrome"
+)
+
+func main() {
+
+	fmt.Println(palindrome.IsPalindrome("sator arepo tenet opera rotas"))
+}
--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -2,6 +2,11 @@ package palindrome
 
 // IsPalindrome accepts a string and returns true if it is a palindrome
 func IsPalindrome(s string) bool {
-
+	if s[0] == s[len(s)-1] {
+		if len(s) <= 2 {
+			return true
+		}
+		return IsPalindrome(s[1 : len(s)-1])
+	}
 	return false
 }
