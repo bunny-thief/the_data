5bd54eab527ecb0b6c2d46b1273a06c3a3ff2127
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2022-03-17 13:30:10 +0000

    Fix base case and move to top of function
    Moved the base case out of the if statement that checks if the first
    character in a word matches the last one. The base case is now at the
    top of the function.
    
    Alo changed the condition of the base case to check if there is one
    character or zero instead of two.

--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -2,11 +2,13 @@ package palindrome
 
 // IsPalindrome accepts a string and returns true if it is a palindrome
 func IsPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+
 	if s[0] == s[len(s)-1] {
-		if len(s) <= 2 {
-			return true
-		}
 		return IsPalindrome(s[1 : len(s)-1])
 	}
+
 	return false
 }
