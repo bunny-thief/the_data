240f2bfe93b23049ae199cfc5ece096c31a7a035
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2022-03-17 13:13:52 +0000

    Add main.go with calls to IsPalindrome

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func main() {
+	sator := "sator arepo tenet opera rotas"
+	fmt.Printf("%q is a palindrome: %v\n", sator, palindrome.IsPalindrome(sator))
 
-	fmt.Println(palindrome.IsPalindrome("sator arepo tenet opera rotas"))
+	race := "racecar"
+	fmt.Printf("%q is a palindrome: %v\n", race, palindrome.IsPalindrome(race))
+
+	rat := "rattata"
+	fmt.Printf("%q is a palindrome: %v\n", rat, palindrome.IsPalindrome(rat))
 }
